4cceb36ca5bdb64051de51b3c87d8900ecb300b7
Ekinxploid <ekinmedya22@gmail.com>
2025-06-17 21:56:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function refreshDiscordProfile() {
     const discordProfileImg = document.querySelector('.discord-profile img');
     const timestamp = new Date().getTime();
-    discordProfileImg.src = `https://lanyard.cnrad.dev/api/1090629202845372477?showDisplayName=true&t=${timestamp}&idleMessage=</> Kaybetmek yok lugatımda </>`;
+    discordProfileImg.src = ````https://discord.com/channels/@me/1384200306736238632/1384605006652510289```&t=${timestamp}&idleMessage=</> Cheyza Özeldir. </>`;
 }
 
 
